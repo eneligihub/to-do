045876df341bbecddb255c5ef7a6f8a9bcfa6941
eneligihub <152338273+eneligihub@users.noreply.github.com>
2024-08-08 14:43:23 +0300

    Revert "Update modules.py"

--- events/models.py
+++ events/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 class Location(models.Model):
     name = models.CharField(max_length=100)
@@ -7,16 +6,15 @@ class Location(models.Model):
 
     def __str__(self):
         return self.name
+class UserProfile(models.Model):
+    first_name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=100)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
 
-# class UserProfile(models.Model):
-#     first_name = models.CharField(max_length=100)
-#     surname = models.CharField(max_length=100)
-#     email = models.EmailField()
-#     phone = models.CharField(max_length=100)
-#     location = models.ForeignKey(Location, on_delete=models.CASCADE)
-#
-#     def __str__(self):
-#         return self.first_name
+    def __str__(self):
+        return self.first_name
 
 class Event_catagory(models.Model):
     name = models.CharField(max_length=100)
@@ -27,32 +25,26 @@ class Event_catagory(models.Model):
 class Event(models.Model):
     category = models.ForeignKey(Event_catagory, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
-    name = models.CharField(max_length=100, blank=False, null=False)
+    name = models.CharField(max_length=100)
     description = models.TextField()
-    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    creator = models.ForeignKey(UserProfile , on_delete=models.CASCADE)
     start_date = models.DateField()
-    end_date = models.DateField(blank=True, null=True)  # can be optional
+    end_date = models.DateField()  # can be optional
     start_time = models.TimeField()
-    end_time = models.TimeField(blank=True, null=True)  # can be optional
-    # location = models.ForeignKey(Location, on_delete=models.CASCADE, blank=False, null=False)
-    is_public = models.BooleanField(default=True)
-    # need to add: pic/video
+    end_time = models.TimeField()  # can be optional
 
+    # need to add: pic/video
+    # public or private
 
 
 def __str__(self):
     return self.name
 
 
-
 class CartItem(models.Model):
-    # time = models.DateField()
     title = models.CharField(max_length=255)
     description = models.TextField()
     event = models.ForeignKey(Event, on_delete=models.CASCADE, blank=True, null=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
 
-
-def __str__(self):
-    return self.title
 
--- events/views.py
+++ events/views.py
@@ -1,5 +1,3 @@
 from django.shortcuts import render
-from django.db import models
-from django.http import HttpResponse
 
 # Create your views here.
